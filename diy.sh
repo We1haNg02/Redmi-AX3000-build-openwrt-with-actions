e409be24128ded351a6677695d77c1208e0ab060
haNg <Liaoweihang.594@gmail.com>
2026-02-18 23:24:49 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,9 +11,15 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 #sed -i 's/ImmortalWrt/ZWRT/g' package/base-files/files/bin/config_generate
 
 # 添加额外的软件包，echo 方式和git clone 方式二选一即可
-#echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-#git clone https://github.com/kenzok8/small.git package/small
-#git clone https://github.com/SunBK201/UA3F.git package/UA3F
-git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
+echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+#src-git luci https://github.com/coolsnowwolf/luci.git' >>feeds.conf.default
+echo 'src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05' >>feeds.conf.default
+#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-24.10
+echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+echo 'src-git telephony https://github.com/coolsnowwolf/telephony.git' >>feeds.conf.default
+#src-git helloworld https://github.com/fw876/helloworld.git
+#src-git qmodem https://github.com/FUjr/modem_feeds.git
+#src-git video https://github.com/openwrt/video.git
+#src-git targets https://github.com/openwrt/targets.git
+#src-git oldpackages http://git.openwrt.org/packages.git
+#src-link custom /usr/src/openwrt/custom-feed
